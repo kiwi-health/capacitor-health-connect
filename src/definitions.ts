a27107faaa0a952b6b0f923321613b679e70f1f1
Marco Hochmuth <marco.hochmuth@marcodes.com>
2025-08-20 21:49:08 +0200

    Version 0.0.32: Fix unit of `BodyWaterMass.mass`

--- src/definitions.ts
+++ src/definitions.ts
@@ -156,7 +156,7 @@ export type Record =
       type: 'BodyWaterMass';
       time: Date;
       zoneOffset?: string;
-      mass: Mass;
+      mass: Percentage;
     }
   | {
       type: 'BoneMass';
